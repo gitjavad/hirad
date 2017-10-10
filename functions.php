603f4d2f45d18318c719066512f833f50a0ad6c3
hirad <m.j.ahmadi88@gmail.com>
2017-10-10 16:07:17 +0330

    n

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( function_exists('register_sidebar') ) {
   wp_enqueue_script( 'semantic.min', get_template_directory_uri() . '/js/semantic.min.js', array ( 'jquery' ), 1.1, true);
 
   wp_enqueue_script( 'dropdown', get_template_directory_uri() . '/js/dropdown.js', array ( 'jquery' ), 1.1, true);
- wp_enqueue_script( 'mosaic', get_template_directory_uri() . '/js/modernizr.custom.js', array ( 'jquery' ), 1.1, true);
+ /*wp_enqueue_script( 'mosaic', get_template_directory_uri() . '/js/modernizr.custom.js', array ( 'jquery' ), 1.1, true);*/
   wp_enqueue_script( 'mosaic', get_template_directory_uri() . '/js/mosaic.js', array ( 'jquery' ), 1.1, true);
 	 wp_enqueue_script( 'mosaic', get_template_directory_uri() . '/js/ cbpShop.js', array ( 'jquery' ), 1.1, true);
 	 
