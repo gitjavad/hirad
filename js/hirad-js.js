00efb6fcdfb3c8a00ff2cc94a19aa918d000cfce
hirad <m.j.ahmadi88@gmail.com>
2017-10-11 00:30:38 +0330

    n

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -14,7 +14,7 @@ document.getElementById("logo_home").style.cssText = 'display:none';
     });
 	
   $(".cbp-pgrotate").click(function(){
-	 $(".box_shop_brand").className += ' box_shop_rotate' 
+	 $(".box_shop_brand").className += ' box_shop_rotate';
   });
     $( window ).on( "orientationchange", function( event ) {
         location.reload();
