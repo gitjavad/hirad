a0a1b5f984b0e6fdfb19c8123b91aa75fd0edc14
hirad <m.j.ahmadi88@gmail.com>
2017-10-10 13:22:57 +0330

    new

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($){
-  
+  var icon = document.getElementById("icon_list");
     document.getElementById("shoping-content").style.cssText= 'display:none' ;
     $("#shoping").click(function(){
+		icon.remove();
         if (document.getElementById("rev_slider_1_2_wrapper")!= null)
             document.getElementById("rev_slider_1_2_wrapper").remove();
         document.getElementById("shoping-content").style.cssText= 'display:block !important' ;
      
-        document.getElementById("icon_list").removeClass("animated");
+        
 document.getElementById("logo_home").style.cssText = 'display:none';
 
     });
