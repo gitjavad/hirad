e1901e83dccca1c101106c9cfb570a2b05ef9b2a
hirad <m.j.ahmadi88@gmail.com>
2017-10-10 12:26:07 +0330

    new

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -1,44 +1,16 @@
 jQuery(document).ready(function($){
-    // now you can use jQuery code here with $ shortcut formatting
-    // this will execute after the document is fully loaded
-    // anything that interacts with your html should go here
+  
     document.getElementById("shoping-content").style.cssText= 'display:none' ;
     $("#shoping").click(function(){
         if (document.getElementById("rev_slider_1_2_wrapper")!= null)
             document.getElementById("rev_slider_1_2_wrapper").remove();
         document.getElementById("shoping-content").style.cssText= 'display:block !important' ;
-        document.getElementById("parquet").style.cssText = 'display:none';
-        document.getElementById("wallcover").style.cssText = 'display:none';
-        document.getElementById("floorCovering").style.cssText = 'display:none';
-        document.getElementById("decorativetools").style.cssText = 'display:none';
+      
         
 document.getElementById("logo_home").style.cssText = 'display:none';
 
     });
-    $("#parqet").click(function () {
-        if (document.getElementById("rev_slider_1_2_wrapper")!= null)
-            document.getElementById("rev_slider_1_2_wrapper").remove();
-    	document.getElementById("shoping-content").style.cssText = 'display: none';
-		document.getElementById("parquet").style.cssText = 'display:block !important';
-    });
-    $("#divarkoob").click(function () {
-        if (document.getElementById("rev_slider_1_2_wrapper")!= null)
-            document.getElementById("rev_slider_1_2_wrapper").remove();
-    	document.getElementById("shoping-content").style.cssText = 'display: none !important';
-		document.getElementById("wallcover").style.cssText = 'display:block ';
-    });
-    $("#kafpoosh").click(function () {
-        if (document.getElementById("rev_slider_1_2_wrapper")!= null)
-            document.getElementById("rev_slider_1_2_wrapper").remove();
-    	document.getElementById("shoping-content").style.cssText = 'display: none !important';
-		document.getElementById("floorCovering").style.cssText = 'display:block ';
-    });
-    $("#abzaredeq").click(function () {
-        if (document.getElementById("rev_slider_1_2_wrapper")!= null)
-            document.getElementById("rev_slider_1_2_wrapper").remove();
-    	document.getElementById("shoping-content").style.cssText = 'display: none !important';
-		document.getElementById("decorativetools").style.cssText = 'display:block ';
-    });
+  
     $( window ).on( "orientationchange", function( event ) {
         location.reload();
     });
