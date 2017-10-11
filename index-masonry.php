099a1fc60fc8f1be13e92b772fcdd157de8a2c8a
hirad <m.j.ahmadi88@gmail.com>
2017-10-11 12:29:00 +0330

    n

--- index-masonry.php
+++ index-masonry.php
@@ -87,19 +87,19 @@
 					<div>
 					<ul>
 						<li>
-							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_1.jpg" width="253px">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_3.jpg" width="253px">
 						</li>
 						<li>
-							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_1.jpg" width="253px">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_3.jpg" width="253px">
 						</li>
 						<li>
-							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_1.jpg" width="253px">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_3.jpg" width="253px">
 						</li>
 						<li>
-							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_1.jpg" width="253px">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_3.jpg" width="253px">
 						</li>
 						<li>
-							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_1.jpg" width="253px">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/esil_3.jpg" width="253px">
 						</li>
 					</ul>
 						
