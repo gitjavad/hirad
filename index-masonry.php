e9420e87c239a14cc7b826f6345f318eaa1e9f25
hirad <m.j.ahmadi88@gmail.com>
2017-10-22 18:52:57 +0330

    m

--- index-masonry.php
+++ index-masonry.php
@@ -88,7 +88,7 @@
                     global $wpdb;
                     $result = $wpdb->get_results( "SELECT COUNT(*), company FROM wp_hirad_shoping GROUP BY company" );
                     foreach ($result as $row){
-                       $res=$row->company;
+                       array_push($res,$res=$row->company);
 
                     }
                     echo "<p>{json_encode($res)}</p>";
