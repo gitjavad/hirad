238a31a55f759e5fdb7066434bcb6321c03f28a7
hirad <m.j.ahmadi88@gmail.com>
2017-10-22 11:24:05 +0330

    m

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -15,8 +15,8 @@ document.getElementById("logo_home").style.cssText = 'display:none';
 	var brn_rot =$(".blb_rotate");
 
 
-    var brand=document.getElementById('cc')
-    console.log(brand.className)
+    var brand=document.getElementsByClassName('cbp-pgitem')
+    console.log(brand.id)
  brn_rot.on("click",function(event){
      event.preventDefault();
 
