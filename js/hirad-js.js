e4861564cb191618d7f99576224b1eb3e880ee4d
hirad <m.j.ahmadi88@gmail.com>
2017-10-22 11:24:45 +0330

    m

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -16,7 +16,7 @@ document.getElementById("logo_home").style.cssText = 'display:none';
 
 
     var brand=document.getElementsByClassName('cbp-pgitem')
-    console.log(brand.id)
+    brand.className+='ss'
  brn_rot.on("click",function(event){
      event.preventDefault();
 
