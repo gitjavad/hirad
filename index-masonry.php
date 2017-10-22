1ac573e6319770f5c2a0f975b680046c7ea8bd4a
hirad <m.j.ahmadi88@gmail.com>
2017-10-22 13:03:39 +0330

    m

--- index-masonry.php
+++ index-masonry.php
@@ -99,7 +99,7 @@
                             $res[]=$row['company'];
                         }
                     echo "<script>console.log('".json_encode($res)."')</script>";
-                    echo "<script>console.log('hi')</script>";
+                    echo "<script>console.log('". $db_table_name."')</script>";
 
                     ?>
 					<!--<div>Esil</div>
