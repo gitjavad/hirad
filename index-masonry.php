9804f539e214fcbba46c4c8395c1c78fbbf24294
hirad <m.j.ahmadi88@gmail.com>
2017-10-22 18:28:57 +0330

    m

--- index-masonry.php
+++ index-masonry.php
@@ -86,7 +86,8 @@
                     <?php
                     $res=array();
                     global $wpdb;
-
+                    $user_count = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->users" );
+                    echo "<p>User count is {$user_count}</p>";
 
 
                     ?>
