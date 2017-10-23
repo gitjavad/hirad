cd344175e2d74d0c7bb3f36aa298245991d308c6
hirad <m.j.ahmadi88@gmail.com>
2017-10-23 11:19:08 +0330

    m

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -34,7 +34,7 @@ document.getElementById("logo_home").style.cssText = 'display:none';
 
     $('.bk_blb').on('click',function (e) {
         e.preventDefault();
-       console.log( e.target.nextElementSibling.lastElementChild.firstElementChild.className)
+       console.log( e.target.nextElementSibling.lastElementChild.firstElementChild.innerHTML)
     })
 
     $( window ).on( "orientationchange", function( event ) {
