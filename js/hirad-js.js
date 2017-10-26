585e000b5870b7d0dc1debec5945f0d5e15ada89
hirad <m.j.ahmadi88@gmail.com>
2017-10-26 20:01:57 +0330

    m

--- js/hirad-js.js
+++ js/hirad-js.js
@@ -18,8 +18,67 @@ jQuery(document).ready(function($){
                 url:'http://hirad-co.com/wp-content/themes/hirad-site/shoping.php?cat='+cbs,
 
                 success:function (data) {
-                    var a=JSON.parse(data)
-                    console.log(Object.keys(a))
+                    var brand_list=JSON.parse(data)
+                    for (var i=0;i<brand_list.length;i++){
+
+                        /*-----start code------*/
+                        $('.main_shop').innerHTML="hi"
+                        /*
+                        echo "<li>
+                   <div class='cbp-pgcontent'>
+                      <span class='bk_blb cbp-pgrotate'>Rotate Item</span>";
+
+            echo "    <div class='cbp-pgitem'>
+                          <div class='box_shop_brand'>
+                            <div class='brand'>{$row->company}</div>
+                            <div class='list_product'>
+                                ";
+
+
+
+
+
+
+            echo      " </div>
+                          </div>
+                      </div>
+                      <ul class='cbp-pgoptions'>
+                            <li class='cbp-pgoptcompare'>Compare</li>
+                            <li class='cbp-pgoptfav'>Favorite</li>
+                            <li class='cbp-pgoptsize'>
+                                <span data-size='XL'>XL</span>
+                                <div class='cbp-pgopttooltip'>
+                                    <span data-size='XL'>XL</span>
+                                    <span data-size='L'>L</span>
+                                    <span data-size='M'>M</span>
+                                    <span data-size='S'>S</span>
+                                </div>
+                            </li>
+                            <li class='cbp-pgoptcolor'>
+                                <span data-color='c1'>Blue</span>
+                                <div class='cbp-pgopttooltip'>
+                                    <span data-color='c1'>Blue</span>
+                                    <span data-color='c2'>Pink</span>
+                                    <span data-color='c3'>Orange</span>
+                                    <span data-color='c4'>Green</span>
+                                </div>
+                            </li>
+                            <li class='cbp-pgoptcart'></li>
+                      </ul>
+                   </div>
+                   <div class='cbp-pginfo'>
+                        <h3>Save my trees</h3>
+                        <span class='cbp-pgprice'>$29</span>
+                   </div>
+
+
+                  </li>
+
+                   "
+                         */
+
+                        /*-----end code--------*/
+                    }
                 }
             })
         })
