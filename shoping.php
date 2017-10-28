6d74ff459b3e3e077530e672ab235a60e2d93f6d
hirad <m.j.ahmadi88@gmail.com>
2017-10-29 00:31:57 +0330

    m

--- shoping.php
+++ shoping.php
@@ -21,7 +21,7 @@ mysqli_set_charset($conn,'utf8');
         if ($result->num_rows>0){
             while ($row=$result->fetch_assoc()){
                 array_push($brand_list,$row['company']);
-                echo $test;
+
 
             }
         }else{
@@ -37,7 +37,7 @@ if ($result2->num_rows>0) {
 }
 $conn->close();
 
-
+echo $test;
 
 
 
